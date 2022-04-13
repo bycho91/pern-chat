41f4a5d8ef0c7f3ab217c8c9867a59caa13da4f4
bycho91 <bycho1991@gmail.com>
2022-04-12 20:53:16 -0500

    finished the login and signup forms with formik

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,82 +1,62 @@
-import {
-  VStack,
-  ButtonGroup,
-  FormControl,
-  FormLabel,
-  Button,
-  FormErrorMessage,
-  Input,
-  Heading,
-} from "@chakra-ui/react";
-import { useFormik } from "formik";
+import { VStack, ButtonGroup, Button, Heading } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
 import * as Yup from "yup";
+import TextField from "./TextField";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const formik = useFormik({
-    initialValues: { username: "", password: "" },
-    validationSchema: Yup.object({
-      username: Yup.string()
-        .required("Field is Required")
-        .min(6, "Username too short")
-        .max(28, "Username too long"),
-      password: Yup.string()
-        .required("Field is Required")
-        .min(6, "Username too short")
-        .max(28, "Username too long"),
-    }),
-    onSubmit: (values, actions) => {
-      alert(JSON.stringify(values, null, 2));
-      actions.resetForm();
-    },
-  });
+  const navigate = useNavigate();
 
   return (
-    <VStack
-      as="form"
-      w={{ base: "90%", md: "500px" }}
-      mx="auto"
-      justify="center"
-      h="100vh"
-      spacing="1rem"
-      onSubmit={formik.handleSubmit}
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .required("Field is Required")
+          .min(6, "Username too short")
+          .max(28, "Username too long"),
+        password: Yup.string()
+          .required("Field is Required")
+          .min(6, "Username too short")
+          .max(28, "Username too long"),
+      })}
+      onSubmit={(values, actions) => {
+        alert(JSON.stringify(values, null, 2));
+        actions.resetForm();
+      }}
     >
-      <Heading mr="auto" size="3xl" mb="1rem">
-        Log In
-      </Heading>
-      <FormControl>
-        <FormLabel fontSize="lg">Username:</FormLabel>
-        <Input
+      <VStack
+        as={Form}
+        w={{ base: "90%", md: "500px" }}
+        mx="auto"
+        justify="center"
+        h="100vh"
+        spacing="1rem"
+      >
+        <Heading mr="auto" size="3xl" mb="1rem">
+          Log In
+        </Heading>
+        <TextField
           name="username"
           placeholder="Enter username"
           autoComplete="off"
-          size="lg"
-          value={formik.values.username}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
+          label="Username"
         />
-        <FormErrorMessage>Invalid Username</FormErrorMessage>
-      </FormControl>
-      <FormControl>
-        <FormLabel fontSize="lg">Password:</FormLabel>
-        <Input
+        <TextField
           name="password"
-          placeholder="Enter Password"
+          placeholder="Enter password"
           autoComplete="off"
-          size="lg"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
+          label="Password"
         />
-        <FormErrorMessage>Invalid Password</FormErrorMessage>
-      </FormControl>
 
-      <ButtonGroup pt="1rem">
-        <Button colorScheme="teal" type="submit">
-          Login
-        </Button>
-        <Button>Create Account</Button>
-      </ButtonGroup>
-    </VStack>
+        <ButtonGroup pt="1rem">
+          <Button colorScheme="teal" type="submit">
+            Login
+          </Button>
+          <Button onClick={() => navigate("/register")}>Create Account</Button>
+        </ButtonGroup>
+      </VStack>
+    </Formik>
   );
 };
 
--- client/src/components/Login/Signup.jsx
+++ client/src/components/Login/Signup.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import { VStack, ButtonGroup, Button, Heading } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import TextField from "./TextField";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const navigate = useNavigate();
+  return (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .required("Field is Required")
+          .min(6, "Username too short")
+          .max(28, "Username too long"),
+        password: Yup.string()
+          .required("Field is Required")
+          .min(6, "Username too short")
+          .max(28, "Username too long"),
+      })}
+      onSubmit={(values, actions) => {
+        alert(JSON.stringify(values, null, 2));
+        actions.resetForm();
+      }}
+    >
+      <VStack
+        as={Form}
+        w={{ base: "90%", md: "500px" }}
+        mx="auto"
+        justify="center"
+        h="100vh"
+        spacing="1rem"
+      >
+        <Heading mr="auto" size="3xl" mb="1rem">
+          Create an Account
+        </Heading>
+        <TextField
+          name="username"
+          placeholder="Enter username"
+          autoComplete="off"
+          label="Username"
+        />
+        <TextField
+          name="password"
+          placeholder="Enter password"
+          autoComplete="off"
+          label="Password"
+        />
+
+        <ButtonGroup pt="1rem">
+          <Button colorScheme="teal" type="submit">
+            Create Account
+          </Button>
+          <Button onClick={() => navigate("/")} leftIcon={<ArrowBackIcon />}>
+            Back
+          </Button>
+        </ButtonGroup>
+      </VStack>
+    </Formik>
+  );
 };
 
 export default Signup;
